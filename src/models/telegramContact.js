dab1e3452d22186eebca73719470758082d8181b
kaleab <your_email@example.com>
2026-05-03 15:23:36 +0300

    qq

--- src/models/telegramContact.js
+++ src/models/telegramContact.js
@@ -1,16 +1,46 @@
-const { registerModel, DataTypes } = require('../config/db');
+const knex = require('../config/db');
 
-// Define TelegramContact model
-const TelegramContact = registerModel('TelegramContact', {
-  chatId: { type: DataTypes.STRING, allowNull: false, unique: true },
-  phone: DataTypes.STRING,
-  username: DataTypes.STRING,
-  linkedUser: { type: DataTypes.INTEGER, defaultValue: null },
-  createdAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
-}, {
-  indexes: [
-    { fields: ['chatId'] }
-  ]
-});
+const TABLE = 'telegram_contacts';
 
-module.exports = TelegramContact;
+module.exports = {
+  // Create contact
+  async create(data) {
+    const [id] = await knex(TABLE).insert(data);
+    return this.findById(id);
+  },
+
+  // Find by ID
+  async findById(id) {
+    return await knex(TABLE)
+      .where({ id })
+      .first();
+  },
+
+  // Find by chatId
+  async findByChatId(chatId) {
+    return await knex(TABLE)
+      .where({ chatId })
+      .first();
+  },
+
+  // Update contact
+  async updateByChatId(chatId, data) {
+    await knex(TABLE)
+      .where({ chatId })
+      .update(data);
+
+    return this.findByChatId(chatId);
+  },
+
+  // Delete
+  async deleteByChatId(chatId) {
+    return await knex(TABLE)
+      .where({ chatId })
+      .del();
+  },
+
+  // Get all
+  async findAll() {
+    return await knex(TABLE).select('*');
+  }
+};
